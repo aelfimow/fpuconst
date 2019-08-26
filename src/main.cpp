61d2c16acfda53304348cc0628efe3b69b0bb032
Alexander Elfimow <my.free.mail@gmx.de>
2019-08-26 21:22:32 +0200

    Print out different PIs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include "fpuconst_pi.h"
 
 int main(int argc, char *argv[])
 try
 {
     argc = argc;
     argv = argv;
+
+    fpuconst_pi<float> pi_float;
+    fpuconst_pi<double> pi_double;
+    fpuconst_pi<long double> pi_long_double;
+
+    std::cout << "pi_float: " << pi_float << std::endl;
+    std::cout << "pi_double: " << pi_double << std::endl;
+    std::cout << "pi_long_double: " << pi_long_double << std::endl;
 
     return EXIT_SUCCESS;
 }
