83af1fc17285645b7fa40a0f596b69cb651d41ae
Alexander Elfimow <my.free.mail@gmx.de>
2019-08-27 20:51:47 +0200

    Specialized templates

--- src/fpuconst_pi.h
+++ src/fpuconst_pi.h
@@ -3,7 +3,9 @@
 #else
 #define FPUCONST_PI_H
 
-extern "C" void fpu_pi(void *p);
+extern "C" void fpu_pi80(void *p);
+extern "C" void fpu_pi64(void *p);
+extern "C" void fpu_pi32(void *p);
 
 template <typename T> class fpuconst_pi
 {
@@ -11,9 +13,45 @@ template <typename T> class fpuconst_pi
         operator T()
         {
             long double value;
-            fpu_pi(&value);
+            fpu_pi80(&value);
 
             return static_cast<T>(value);
+        }
+};
+
+template <> class fpuconst_pi<long double>
+{
+    public:
+        operator long double()
+        {
+            long double value;
+            fpu_pi80(&value);
+
+            return value;
+        }
+};
+
+template <> class fpuconst_pi<double>
+{
+    public:
+        operator double()
+        {
+            double value;
+            fpu_pi64(&value);
+
+            return value;
+        }
+};
+
+template <> class fpuconst_pi<float>
+{
+    public:
+        operator float()
+        {
+            float value;
+            fpu_pi32(&value);
+
+            return value;
         }
 };
 
