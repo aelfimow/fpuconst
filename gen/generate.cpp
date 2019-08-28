8d6622426fa045778bc20584207379de58d0a1f6
Alexander Elfimow <my.free.mail@gmx.de>
2019-08-28 21:41:44 +0200

    gen: function to generate fldz

--- gen/generate.cpp
+++ gen/generate.cpp
@@ -26,6 +26,27 @@ void gen_function_fldpi(std::string const &func_name, r64 &param)
     RET();
 }
 
+template <typename T>
+void gen_function_fldz(std::string const &func_name, r64 &param)
+{
+    comment("void " + func_name + "(void *p)");
+    comment("p is in " + param.name());
+
+    global(func_name);
+
+    section code { ".text" };
+    code.start();
+
+    label(func_name);
+
+    FLDZ();
+
+    T addr { param };
+    FSTP(addr);
+
+    RET();
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -51,6 +72,9 @@ try
         gen_function_fldpi<m80fp>("fpu_pi80", RCX);
         gen_function_fldpi<m64fp>("fpu_pi64", RCX);
         gen_function_fldpi<m32fp>("fpu_pi32", RCX);
+        gen_function_fldz<m80fp>("fpu_zero80", RCX);
+        gen_function_fldz<m64fp>("fpu_zero64", RCX);
+        gen_function_fldz<m32fp>("fpu_zero32", RCX);
     }
 
     if (forLinux)
@@ -58,6 +82,9 @@ try
         gen_function_fldpi<m80fp>("fpu_pi80", RDI);
         gen_function_fldpi<m64fp>("fpu_pi64", RDI);
         gen_function_fldpi<m32fp>("fpu_pi32", RDI);
+        gen_function_fldz<m80fp>("fpu_zero80", RDI);
+        gen_function_fldz<m64fp>("fpu_zero64", RDI);
+        gen_function_fldz<m32fp>("fpu_zero32", RDI);
     }
 
     return EXIT_SUCCESS;
