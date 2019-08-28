6c846583c1e32251bcfa53ca77028ffc67ea32f3
Alexander Elfimow <my.free.mail@gmx.de>
2019-08-28 21:48:02 +0200

    Added templates for getting zero

--- src/fpuconst_pi.h
+++ src/fpuconst_pi.h
@@ -7,6 +7,10 @@ extern "C" void fpu_pi80(void *p);
 extern "C" void fpu_pi64(void *p);
 extern "C" void fpu_pi32(void *p);
 
+extern "C" void fpu_zero80(void *p);
+extern "C" void fpu_zero64(void *p);
+extern "C" void fpu_zero32(void *p);
+
 template <typename T> class fpuconst_pi
 {
     public:
@@ -50,6 +54,54 @@ template <> class fpuconst_pi<float>
         {
             float value;
             fpu_pi32(&value);
+
+            return value;
+        }
+};
+
+template <typename T> class fpuconst_zero
+{
+    public:
+        operator T()
+        {
+            long double value;
+            fpu_zero80(&value);
+
+            return static_cast<T>(value);
+        }
+};
+
+template <> class fpuconst_zero<long double>
+{
+    public:
+        operator long double()
+        {
+            long double value;
+            fpu_zero80(&value);
+
+            return value;
+        }
+};
+
+template <> class fpuconst_zero<double>
+{
+    public:
+        operator double()
+        {
+            double value;
+            fpu_zero64(&value);
+
+            return value;
+        }
+};
+
+template <> class fpuconst_zero<float>
+{
+    public:
+        operator float()
+        {
+            float value;
+            fpu_zero32(&value);
 
             return value;
         }
