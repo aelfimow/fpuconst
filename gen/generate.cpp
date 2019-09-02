fab503fdb9c9698c283030ead56885135afb2c0c
Alexander Elfimow <my.free.mail@gmx.de>
2019-09-02 20:35:57 +0200

    gen: further function to generate FLDLG2

--- gen/generate.cpp
+++ gen/generate.cpp
@@ -110,6 +110,27 @@ void gen_function_fldl2e(std::string const &func_name, r64 &param)
     RET();
 }
 
+template <typename T>
+void gen_function_fldlg2(std::string const &func_name, r64 &param)
+{
+    comment("void " + func_name + "(void *p)");
+    comment("p is in " + param.name());
+
+    global(func_name);
+
+    section code { ".text" };
+    code.start();
+
+    label(func_name);
+
+    FLDLG2();
+
+    T addr { param };
+    FSTP(addr);
+
+    RET();
+}
+
 int main(int argc, char *argv[])
 try
 {
@@ -147,6 +168,9 @@ try
         gen_function_fldl2e<m80fp>("fpu_ld_e_80", RCX);
         gen_function_fldl2e<m64fp>("fpu_ld_e_64", RCX);
         gen_function_fldl2e<m32fp>("fpu_ld_e_32", RCX);
+        gen_function_fldlg2<m80fp>("fpu_lg2_80", RCX);
+        gen_function_fldlg2<m64fp>("fpu_lg2_64", RCX);
+        gen_function_fldlg2<m32fp>("fpu_lg2_32", RCX);
     }
 
     if (forLinux)
@@ -166,6 +190,9 @@ try
         gen_function_fldl2e<m80fp>("fpu_ld_e_80", RDI);
         gen_function_fldl2e<m64fp>("fpu_ld_e_64", RDI);
         gen_function_fldl2e<m32fp>("fpu_ld_e_32", RDI);
+        gen_function_fldlg2<m80fp>("fpu_lg2_80", RDI);
+        gen_function_fldlg2<m64fp>("fpu_lg2_64", RDI);
+        gen_function_fldlg2<m32fp>("fpu_lg2_32", RDI);
     }
 
     return EXIT_SUCCESS;
